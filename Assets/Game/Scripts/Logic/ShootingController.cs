8394a2b0325cb493ca4cc52ec98a358293ca8071
mrterdem <mrterdem1998@hotmail.com>
2026-08-17 19:10:21 +0300

    Fire interval change

--- Assets/Game/Scripts/Logic/ShootingController.cs
+++ Assets/Game/Scripts/Logic/ShootingController.cs
@@ -13,7 +13,7 @@ namespace Game.Scripts.Logic
         [SerializeField] private Board board;
         [SerializeField] private CannonSlotManager cannonSlotManager;
         
-        private const float FIRE_INTERVAL = 0.05f;
+        private const float FIRE_INTERVAL = 0.2f;
         private static readonly WaitForSeconds FireIntervalWait = new(FIRE_INTERVAL);
         
         private readonly HashSet<Cannon> _firingCannons = new();
